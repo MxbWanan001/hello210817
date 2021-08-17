13669a6c244ee7c13de72dacd8629cd731b3700b
xb <xb@xiaobangxhn.com>
2021-08-17 15:24:48 +0800

    上传到本地库

---
+++ src/main/java/com/xiaobang/domain/Student.java
@@ -0,0 +1,43 @@
+package com.xiaobang.domain;
+
+/**
+ * @author xiaobang
+ * @create 2021-08-17 15:22
+ * @Description
+ */
+public class Student {
+    private String name;
+    private String age;
+
+    public Student() {
+    }
+
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
